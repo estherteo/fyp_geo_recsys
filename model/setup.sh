1dc29f36ae6cd9526020c9184014585f26e76e65
esther <104299126+estherteo@users.noreply.github.com>
2025-04-06 02:28:29 +0800

    Update setup.sh

--- model/setup.sh
+++ model/setup.sh
@@ -2,3 +2,5 @@ sudo apt remove python3-blinker -y
 pip install -r ./requirements.txt
 pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/cu128 --force-reinstall
 huggingface-cli download estieeee/yelp2018_processed --local-dir=./data/ --repo-type=dataset
+mkdir ./dataset_challenge
+cp ./data/* ./dataset_challenge/
